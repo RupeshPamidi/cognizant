102fcf9b40ab592258a07a87068756ad1c9a1f39
RupeshPamidi <154714612+RupeshPamidi@users.noreply.github.com>
2026-06-16 12:09:08 +0530

    Delete jQuery directory

--- jQuery/Exercise-01/script.js
+++
@@ -1,4 +0,0 @@
-// Exercise 01 script.js
-$(function(){
-  console.log('Hello World! element text via jQuery:', $('#hello').text());
-});
--- jQuery/Exercise-06/script.js
+++
@@ -1,8 +0,0 @@
-// Exercise 06 script.js
-$(function(){
-  $('#box').on('click', function(){ $(this).css('background','#bbf7d0'); $('#out').text('Box clicked'); });
-  $('#box').on('dblclick', function(){ $(this).css('background',''); $('#out').text('Box double-clicked'); });
-  $('#box').on('mouseenter', function(){ $(this).css('border-color','#f97316'); });
-  $('#box').on('mouseleave', function(){ $(this).css('border-color','#374151'); $('#out').text(''); });
-  $('#txt').on('keypress', function(){ $('#out').text('Typing... ' + $(this).val().length); });
-});
